628423a77c6ec5989e91efb8aee4b05ffcacdfed
Angel <adcalder7@gmail.com>
2020-03-27 22:46:28 -0400

    Done

---
+++ data_structures/hw.py
@@ -0,0 +1,16 @@
+
+
+# Basics with their behaviors (Create, Read, Update, Delete)
+
+# Array
+# Linkelist
+# Stack
+# Queue
+# Set
+# HashMap
+
+# Advance Topics
+# Sort Array
+# Sort Linklist
+# TreeSet
+# TreeMap
